5f029fa5b5c345f931fcdd82ea72ed8c8afb6cc7
Nina Doschek <ndoschek@eclipsesource.com>
2023-04-18 16:20:38 +0200

    Update to latest Theia and GLSP versions
    - Add upgrade scripts for more convenient version updates
    - Align code to updated GLSP version
    
    Part of https://github.com/eclipse-emfcloud/coffee-editor/issues/415

--- client/comparison-extension/src/browser/graphical/graphical-comparison-widget.ts
+++ client/comparison-extension/src/browser/graphical/graphical-comparison-widget.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2021 EclipseSource and others.
+ * Copyright (c) 2021-2023 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -17,7 +17,6 @@ import { FitToScreenAction } from '@eclipse-glsp/client';
 import { GLSPDiagramWidget } from '@eclipse-glsp/theia-integration/lib/browser';
 import { BaseWidget, Message, SplitPanel, StatefulWidget, Title, Widget } from '@theia/core/lib/browser';
 import { inject, injectable } from 'inversify';
-import { DiagramWidget } from 'sprotty-theia';
 import { ComparisonExtensionConfiguration } from '../comparison-extension-configuration';
 
 export const GraphicalComparisonWidgetOptions = Symbol('GraphicalComparisonWidgetOptions');
@@ -124,7 +123,7 @@ export class GraphicalComparisonWidget extends BaseWidget implements StatefulWid
         title.iconClass = `${this.config.getGraphicalDiffViewIcon()} file-icon`;
     }
 
-    get diagramWidget(): DiagramWidget {
+    get diagramWidget(): GLSPDiagramWidget {
         if (this.options) {
             if (this.lastFocusLeft) {
                 return this.options.left;
